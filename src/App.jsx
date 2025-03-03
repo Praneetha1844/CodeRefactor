9d2c59d7eecdb27405b32ab54dc4b144f6269ea5
Praneetha <praneetha1844@gmail.com>
2025-03-03 12:23:40 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className="bg-gray-500 h-screen ">
+      <div>
+        Code Refactor
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
